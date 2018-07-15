dd7b983af428683cd5010cc153df57e46e7fe70a
Evobolics <evobolics@e01d.com>
2018-07-15 12:06:34 -0500

    I further simplified the API.  The most basic four commands can be called with just one line of code.

--- src/E01D.Pal.Apps.Common/Code/Exts/Runtimic/RuntimicContainerExts.cs
+++ src/E01D.Pal.Apps.Common/Code/Exts/Runtimic/RuntimicContainerExts.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection.Emit;
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
 using Root.Code.Containers.E01D.Runtimic;
 using Root.Code.Models.E01D.Runtimic.Execution.Conversion;
 
@@ -19,6 +21,75 @@ namespace Root.Code.Exts.Runtimic
 		public static System.Reflection.Assembly Convert(this RuntimicContainer container, System.Reflection.Assembly assembly)
 		{
 			return container.Api.Runtimic.Execution.Conversion.QuickConvert(assembly);
+		}
+
+		public static Type QuickConvert(this RuntimicContainer container, Type type)
+		{
+			return container.Api.Runtimic.Execution.Conversion.QuickConvert(type);
+		}
+
+		public static Type QuickConvert(this RuntimicContainer container, Type type, out ILConversionResult result)
+		{
+			return container.Api.Runtimic.Execution.Conversion.QuickConvert(type, out result);
+		}
+
+		public static Type[] QuickConvert(this RuntimicContainer container, Type[] types)
+		{
+			return container.Api.Runtimic.Execution.Conversion.QuickConvert(types);
+		}
+
+
+		public static Type[] QuickConvert(this RuntimicContainer container, Type[] types, out ILConversionResult result)
+		{
+			return container.Api.Runtimic.Execution.Conversion.QuickConvert(types, out result);
+		}
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="assembly"></param>
+		/// <returns></returns>
+		public static Assembly QuickConvert(this RuntimicContainer container, Assembly assembly)
+		{
+			return container.Api.Runtimic.Execution.Conversion.QuickConvert(assembly);
+		}
+
+
+		/// <summary>
+		/// Converts the assembly an assembly to a dynamic assembly and returns the conversion result as an out parameter.
+		/// </summary>
+		/// <param name="assembly"></param>
+		/// <param name="result"></param>
+		/// <returns></returns>
+		public static Assembly QuickConvert(this RuntimicContainer container, Assembly assembly,
+			out ILConversionResult result)
+		{
+			return container.Api.Runtimic.Execution.Conversion.QuickConvert(assembly, out result);
+		}
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="assembly"></param>
+		/// <returns></returns>
+		public static Assembly[] QuickConvert(this RuntimicContainer container, Assembly[] assemblies)
+		{
+			return container.Api.Runtimic.Execution.Conversion.QuickConvert(assemblies);
+		}
+
+
+		/// <summary>
+		/// Converts the assembly an assembly to a dynamic assembly and returns the conversion result as an out parameter.
+		/// </summary>
+		/// <param name="assembly"></param>
+		/// <param name="result"></param>
+		/// <returns></returns>
+		public static Assembly[] QuickConvert(this RuntimicContainer container, Assembly[] assemblies,
+			out ILConversionResult result)
+		{
+			return container.Api.Runtimic.Execution.Conversion.QuickConvert(assemblies, out result);
 		}
 	}
 }
---
+++ src/E01D.Pal.Apps.Common/Examples/Example1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Root.Code.Exts.Runtimic;
+using Root.Code.Shortcuts.E01D;
+
+namespace Root.Examples
+{
+	public class Example1
+	{
+		public void Perform()
+		{
+			// Example1: Convert a type
+			var convertedType = EvoAssembly.QuickConvert(typeof(Example1));
+
+			// Example2: Convert an assembly
+			var convertedAssembly = EvoAssembly.QuickConvert(typeof(Example1).Assembly);
+		}
+	}
+}
